2ae5c2bbefe4ed21bc97576684a1202d44eb0d82
Dwi Fahni Denni (dfdenni) <dfdenni0912@gmail.com>
2022-12-18 12:42:00 +0700

    Release/v3.4 (#16)
    * Update terraform resources eks, manifest, node group, autoscale & taggination
    
    * Refactoring variables resources
    
    * Refactoring variables resources ec2. Added new resources ec2-nifi
    
    * Added new resources rds for laraveldb, nifidb, refactoring goapp-to-goappdb
    
    * Update changelogs history
    
    * Update list resources terraform
    
    * Update tags badges
    
    * Update badges version download
    
    * Change permission files & folders
    
    * Files & Folders
    
    * Refactoring script for build, tag, push & pull container

--- dockerhub-push.sh
+++ dockerhub-push.sh
@@ -11,7 +11,7 @@ set -e
 # export CI_PROJECT_NAME="terraform-infra"
 
 # export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
-export IMAGE=$2
+export IMAGE=$1
 
 login_docker() {
   echo '==================='
@@ -23,8 +23,8 @@ login_docker() {
 }
 
 docker_push() {
-  export TAGS_ID=$1
-  IMAGES=`docker images --format "{{.Repository}}:{{.Tag}}" | grep $IMAGE:${TAGS_ID}`
+  export TAGS_ID=$2
+  IMAGES=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep $IMAGE:${TAGS_ID})
   for IMG in $IMAGES; do
     echo "Docker Push => $IMG"
     echo ">> docker push $IMG"
@@ -36,9 +36,7 @@ docker_push() {
 
 main() {
   login_docker
-  # docker_push alpine devopscorner/terraform-infra
-  # docker_push ubuntu devopscorner/terraform-infra
-  # docker_push codebuild devopscorner/terraform-infra
+  # docker_push devopscorner/terraform-infra [alpine|ubuntu|codebuild|version|latest|tags|custom-tags]
   docker_push $1 $2
   echo ''
   echo '-- ALL DONE --'
@@ -46,3 +44,6 @@ main() {
 
 ### START HERE ###
 main $1 $2
+
+### How to Execute ###
+# ./dockerhub-push.sh [DOCKERHUB_IMAGE_PATH] [alpine|ubuntu|codebuild|version|latest|tags|custom-tags]
--- dockerhub-tag.sh
+++ dockerhub-tag.sh
@@ -11,50 +11,48 @@ set -e
 # export CI_PROJECT_NAME="terraform-infra"
 
 # export IMAGE="$CI_PROJECT_PATH/$CI_PROJECT_NAME"
-export IMAGE=$3
-
-# export CICD_VERSION="1.0.5"
-# export ALPINE_VERSION="3.16"
-# export UBUNTU_VERSION="22.04"
-# export CODEBUILD_VERSION="4.0"
+export IMAGE=$1
 
 set_tag() {
-  export TAGS_ID=$1
-  export CUSTOM_TAGS=$2
-  export BASE_IMAGE="$IMAGE:${TAGS_ID}"
-  export COMMIT_HASH=`git log -1 --format=format:"%H"`
+  export BASE_IMAGE=$2
+  export TAGS_ID=$3
+  export CUSTOM_TAGS=$4
+  export COMMIT_HASH=$(git log -1 --format=format:"%H")
 
   if [ "$CUSTOM_TAGS" = "" ]; then
     export TAGS="${TAGS_ID} \
-    ${TAGS_ID}-latest \
-    ${TAGS_ID}-${COMMIT_HASH}"
+    ${BASE_IMAGE}-${TAGS_ID} \
+    ${TAGS_ID}-${COMMIT_HASH} \
+    ${BASE_IMAGE}-${COMMIT_HASH} "
   else
     export TAGS="${TAGS_ID} \
-    ${TAGS_ID}-latest \
-    ${TAGS_ID}-${CUSTOM_TAGS} \
-    ${TAGS_ID}-${COMMIT_HASH}"
+    ${BASE_IMAGE}-${TAGS_ID} \
+    ${TAGS_ID}-${COMMIT_HASH} \
+    ${BASE_IMAGE}-${COMMIT_HASH} \
+    ${TAGS_ID}-${CUSTOM_TAGS}"
   fi
 }
 
 docker_tag() {
   for TAG in $TAGS; do
     echo "Docker Tags => $IMAGE:$TAG"
-    echo ">> docker tag $BASE_IMAGE $IMAGE:$TAG"
-    docker tag $BASE_IMAGE $IMAGE:$TAG
+    echo ">> docker tag $IMAGE:$BASE_IMAGE $IMAGE:$TAG"
+    docker tag $IMAGE:$BASE_IMAGE $IMAGE:$TAG
     echo '- DONE -'
     echo ''
   done
 }
 
 main() {
-  # set_tag alpine 3.16 devopscorner/terraform-infra
-  # set_tag ubuntu 22.04 devopscorner/terraform-infra
-  # set_tag codebuild 4.0 devopscorner/terraform-infra
-  set_tag $1 $2 $3
+  # set_tag devopscorner/terraform-infra [alpine|ubuntu|codebuild] [version|latest|tags] [custom-tags]
+  set_tag $1 $2 $3 $4
   docker_tag
   echo ''
   echo '-- ALL DONE --'
 }
 
 ### START HERE ###
-main $1 $2 $3
+main $1 $2 $3 $4
+
+### How to Execute ###
+# ./dockerhub-tag.sh [DOCKERHUB_IMAGE_PATH] [alpine|ubuntu|codebuild] [version|latest|tags] [custom-tags]
