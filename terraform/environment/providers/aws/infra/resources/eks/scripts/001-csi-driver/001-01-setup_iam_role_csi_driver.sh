1dcdfa2c98b79fa042cf0a64bcab04aceaa9e629
Dwi Fahni Denni (dfdenni) <dfdenni0912@gmail.com>
2023-05-09 18:19:36 +0700

    Develop (#24)
    * Refactoring budget terraform script
    
    * Added CI/CD terraform
    
    * Refactoring cloud 9 terraform
    
    * Refactoring ec2 terraform
    
    * Update monitoring terraform provisioning
    
    * Refactoring MWAA terraform
    
    * Added EKS ADOT Collector terraform script
    
    * Added backend storage terraform state
    
    * Refactoring terraform EKS
    
    * Update version history
    
    * Fixing hash id

--- terraform/environment/providers/aws/infra/resources/eks/scripts/001-csi-driver/001-01-setup_iam_role_csi_driver.sh
+++ terraform/environment/providers/aws/infra/resources/eks/scripts/001-csi-driver/001-01-setup_iam_role_csi_driver.sh
@@ -52,7 +52,7 @@ cat <<EOF > kms-key-for-encryption-on-ebs.json
         "kms:ListGrants",
         "kms:RevokeGrant"
       ],
-      "Resource": ["arn:aws:kms:ap-southeast-1:YOUR_AWS_ACCOUNT:key/4d7ef7e8-cef9-4482-bc2e-87c742544ee5"],
+      "Resource": ["arn:aws:kms:ap-southeast-1:YOUR_AWS_ACCOUNT:key/HASH_ID"],
       "Condition": {
         "Bool": {
           "kms:GrantIsForAWSResource": "true"
@@ -68,7 +68,7 @@ cat <<EOF > kms-key-for-encryption-on-ebs.json
         "kms:GenerateDataKey*",
         "kms:DescribeKey"
       ],
-      "Resource": ["arn:aws:kms:ap-southeast-1:YOUR_AWS_ACCOUNT:key/4d7ef7e8-cef9-4482-bc2e-87c742544ee5"]
+      "Resource": ["arn:aws:kms:ap-southeast-1:YOUR_AWS_ACCOUNT:key/HASH_ID"]
     }
   ]
 }
